00df56135888e04d079e38eebe5b2bc30c6ed946
代波 <jack._2004@163.com>
2016-09-21 20:33:54 +0800

    创建自定义NSOperation

---
+++ DBWebImage/DBWebImage/DBWebImage/DownloaderOperation.h
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.h
+//  DBWebImage
+//
+//  Created by 代波 on 16/9/21.
+//  Copyright © 2016年 daibo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloaderOperation : NSOperation
+
+@end
---
+++ DBWebImage/DBWebImage/DBWebImage/DownloaderOperation.m
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.m
+//  DBWebImage
+//
+//  Created by 代波 on 16/9/21.
+//  Copyright © 2016年 daibo. All rights reserved.
+//
+
+#import "DownloaderOperation.h"
+
+@implementation DownloaderOperation
+
+@end
